62b080a88d2f032d499388c22969e5e5359dd6b2
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-10-10 16:07:59 +0900

    微調整

--- Wall.cpp
+++ Wall.cpp
@@ -23,8 +23,10 @@ void Wall::DrawQuad(Corners a, int imageGH, int imageWidth, int imageHeight, uns
 void Wall::Stage1Initialize()
 {
 	wallNum_ = 10;
-	float heightTotal = 400;
+	float heightTotal = 800;
 	float distance = 150;
+	
+
 
 	for (int i = 0;i < 28;i++) {
 		pos_[i] = {};
@@ -38,28 +40,29 @@ void Wall::Stage1Initialize()
 	height_[0] = 20.0f;
 	height_[2] = 80.0f;
 	height_[4] = 140.0f;
-	height_[6] = 170.0f;
+	height_[6] = 200.0f;
 	height_[8] = 260.0f;
 
 	//上の壁の高さ
 	height_[1] = heightTotal - height_[0];
+
 	height_[3] = heightTotal - height_[2];
 	height_[5] = heightTotal - height_[4];
 	height_[7] = heightTotal - height_[6];
 	height_[9] = heightTotal - height_[8];
 
 	//下の壁の位置
-	pos_[0].x =400.0f;
-	pos_[2].x = 600.0f;
-	pos_[4].x = 1000.0f;
-	pos_[6].x = 1500.0f;
-	pos_[8].x = 2000.0f;
+	pos_[0].x =700.0f;
+	pos_[2].x = 1200.0f;
+	pos_[4].x = 1700.0f;
+	pos_[6].x = 2200.0f;
+	pos_[8].x = 2700.0f;
 
 	
 
 	for (int i = 0;i < wallNum_;i++) {
 		pos_[i].y = 700.0f - height_[i] / 2.0f;
-		width_[i] = 25.0f;
+		width_[i] = 10.0f;
 	}
 
 	for (int i = 1;i < wallNum_;i += 2) {
